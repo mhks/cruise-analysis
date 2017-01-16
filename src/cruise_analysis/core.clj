36d77c669c0c46f54c6b920f8fd100b0b4378588
Michael Hicks <hicmic25@gmail.com>
2017-01-15 20:44:38 -0800

    add plot-radius-factor function

--- src/cruise_analysis/core.clj
+++ src/cruise_analysis/core.clj
@@ -3,8 +3,6 @@
   (:require [clojure.math.numeric-tower :as math]
             [clojure.math.combinatorics :as combo]))
 
-
-
 (defn plot-area
   "Returns area of tree plot in acres"
   [[prf dbh]]
@@ -12,6 +10,12 @@
    (/ (* pi (math/expt (* prf dbh) 2)) 43650)))
 
 (plot-area [2.75 10])
-(plot-area [2.75 32])
+(plot-area [2.75 5])
+
+(defn plot-radius-factor
+  [baf]
+  (let [pi 3.141593]
+  (math/sqrt (/ 238 (* baf pi)))))
+
 
 
